da215a540582ed17aa0509e901514cbb33827e74
DenisLazarian <denis.lazarian.emanuel@gmail.com>
2023-07-28 00:23:43 +0200

    feat: Adding initial files for the Angular project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {Route, Routes} from "@angular/router";
+import { HomeComponent } from './home/home.component';
+
+
+const appModule: Routes = [
+  // {path:'', component: HomeComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
