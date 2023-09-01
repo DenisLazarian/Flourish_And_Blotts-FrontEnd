ee85f060606b5095d29700c6edfa9b700d3b4564
DenisLazarian <denis.lazarian.emanuel@gmail.com>
2023-09-01 18:29:42 +0200

    feat(api): Adding Login form
    Adding the login component for authentication on page.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from "./home/home.component";
 import {ReadCSVFileComponent} from "./read-csvfile/read-csvfile.component";
 import {AuthModule} from "./modules/auth/auth.module";
+import {RegisterModule} from "./modules/register/register.module";
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
@@ -15,7 +16,13 @@ const routes: Routes = [
   {
     path: 'home',
     loadChildren: () => import(`./modules/home/home.module`).then(m=>m.HomeModule),
+  },
+  {
+    path: 'registration',
+    loadChildren: () => import(`./modules/register/register.module`).then(m=>m.RegisterModule)
   }
+
+
 ]
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {Route, RouterModule, Routes} from "@angular/router";
 import { HomeComponent } from './home/home.component';
-import { Pagina2Component } from './pagina2/pagina2.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {HttpClient, HttpClientModule} from "@angular/common/http";
 import { ReadCSVFileComponent } from './read-csvfile/read-csvfile.component';
 // import { UserComponent } from './core/models/user/user.component';
 import {AuthModule} from "./modules/auth/auth.module";
 import {HomeModule} from "./modules/home/home.module";
+import { RegistrationComponent } from './modules/register/registration/registration.component';
 
 
 
@@ -20,8 +20,8 @@ import {HomeModule} from "./modules/home/home.module";
   declarations: [
     AppComponent,
     HomeComponent,
-    Pagina2Component,
     ReadCSVFileComponent,
+    RegistrationComponent,
   ],
   imports: [
     AuthModule,
